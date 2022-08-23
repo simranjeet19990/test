f701959523d2477142c416bb1a8781a21ffe21af
simranjeet19990 <simranhimself@gmail.com>
2022-08-23 17:52:14 +0530

    class Lottery

---
+++ opaly/test2.py
@@ -0,0 +1,68 @@
+#Here is tempary idea for random numbergame 
+"""import random
+print("Enter any number between 0 to 20")    
+choss=input()
+gauss=random.randint(0,20)
+
+if gauss==choss:
+    print("you are pass")
+
+elif gauss!=choss:
+        print("you are fail try again")
+""" 
+#here we useing loop for repting the program
+"""
+  
+import random
+gauss=random.randint(0,20)
+print('Number',gauss)
+
+attemt=5
+while attemt>0:
+    choss=int(input('Enter any number b/w 0 to20:'))
+    if gauss==choss:
+        print('you win')
+    
+        break
+    elif gauss!=choss:
+        print(f'try again {attemt} attemt left')
+        attemt-=1
+        #print('you lost')
+        continue
+print('know game is over')   """
+# NAME OF THE GAME IS (LOTTERY MACHINE)
+import random
+class Lottery():
+    # def __init__(self,number):
+    #     self.number=number
+       
+    #gauss=random.randint(0,20)
+    #choss=int(input('Enter any number b/w 0 to20:'))
+    #attemt=5
+
+    def my(self):
+        Gauss=random.randint(0,20)
+        
+        Attemt=5
+        while Attemt>0:
+            number=int(input('Enter any number b/w 0 to20:'))
+            if Gauss==number:
+                print("you win")
+                break
+        
+            elif Gauss!=number:
+                print(f'try again {Attemt} attemt left')
+                Attemt-=1
+                continue
+            
+        print('know game is over')
+
+x=Lottery()
+x.my()
+
+
+
+    
+
+
+    
